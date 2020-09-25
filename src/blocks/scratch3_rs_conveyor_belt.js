08d0e187d4c9af6478658369dedbedf6f38af01f
liumingzw <374406742@qq.com>
2020-09-25 15:55:38 +0800

    删除无用代码

--- src/blocks/scratch3_rs_conveyor_belt.js
+++ src/blocks/scratch3_rs_conveyor_belt.js
@@ -5,17 +5,9 @@ class Blocks {
 
     getPrimitives() {
         return {
-            RS_CONVEYOR_BELT_SET_ACCELERATION: this.RS_CONVEYOR_BELT_SET_ACCELERATION,
             RS_CONVEYOR_BELT_MOVE: this.RS_CONVEYOR_BELT_MOVE,
             RS_CONVEYOR_BELT_STOP: this.RS_CONVEYOR_BELT_STOP,
         };
-    }
-
-    RS_CONVEYOR_BELT_SET_ACCELERATION(args) {
-        const blockName = "RS_CONVEYOR_BELT_SET_ACCELERATION";
-        return new Promise((resolve) => {
-            this.runtime.emit("rotrics-async", {blockName, resolve, args})
-        });
     }
 
     RS_CONVEYOR_BELT_MOVE(args) {
